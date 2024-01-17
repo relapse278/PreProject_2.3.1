057e4c5b3eb2aef3b5cf50a10d67134634248271
Руслан Шевченко <r.schewtschenko@mail.ru>
2024-01-17 07:58:48 +0700

    Altered the UserDaoImpl class and the User class due to the annotations of the mentor. The DbConfig class is still in construction, so it wasn't committed.

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,6 +1,7 @@
 package web.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -25,6 +26,20 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email);
     }
 
     public Long getId() {
